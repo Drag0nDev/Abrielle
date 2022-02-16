4e7643c69efd6026798022edb57f8595dc12c7ba
drag0nDev <lennvh046@gmail.com>
2022-02-16 16:34:46 +0100

    admin only command to let the bot say what the admin wants the bot to say

---
+++ src/main/java/Abrielle/bot/Commands/Commands/admin/CmdSay.java
@@ -0,0 +1,91 @@
+package Abrielle.bot.Commands.Commands.admin;
+
+import Abrielle.bot.Abrielle;
+import Abrielle.bot.Commands.Command;
+import Abrielle.constants.Colors;
+import Abrielle.util.Exceptions.AbrielleException;
+import com.github.rainestormee.jdacommand.CommandAttribute;
+import com.github.rainestormee.jdacommand.CommandDescription;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.InteractionHook;
+
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.StringJoiner;
+
+import static Abrielle.util.utils.Utils.getChannel;
+
+@CommandDescription(
+        name = "say",
+        description = "Sends a message in the specified channel.",
+        triggers = {"say"},
+        attributes = {
+                @CommandAttribute(key = "category", value = "admin"),
+                @CommandAttribute(key = "usage", value = "[command | alias] [channel] [message]"),
+                @CommandAttribute(key = "examples", value = "'a!say #general hi mom!'\n" +
+                        "'a!say 418037700751261708 hi mom!'"),
+        }
+)
+
+public record CmdSay(Abrielle bot) implements Command {
+
+    @Override
+    public void runSlash(Guild guild, TextChannel tc, Member member, SlashCommandInteractionEvent event, InteractionHook hook) throws Exception {
+        BaseGuildMessageChannel channel;
+
+        channel = event.getOption("channel").getAsTextChannel();
+
+        if (channel == null)
+            channel = event.getOption("channel").getAsNewsChannel();
+
+        String msg = Objects.requireNonNull(event.getOption("message")).getAsString();
+
+        if (channel == null)
+            throw new AbrielleException("Channel does not exist!");
+
+        channel.sendMessage(msg).queue();
+        hook.sendMessageEmbeds(
+                        new EmbedBuilder()
+                                .setColor(Colors.NORMAL.getCode())
+                                .setTimestamp(ZonedDateTime.now())
+                                .setDescription("Message sent succesefully!")
+                                .addField("Channel", channel.getAsMention(), false)
+                                .addField("Message", msg, false)
+                                .build())
+                .queue();
+    }
+
+    @Override
+    public void runCommand(Message msg, Guild guild, TextChannel tc, Member member) throws Exception {
+        String[] args = bot.getArguments(msg);
+        String[] msgToSend = new String[args.length - 1];
+
+        BaseGuildMessageChannel channel = getChannel(msg, guild, args, 0);
+
+        System.arraycopy(args, 1, msgToSend, 0, args.length - 1);
+        String msgComplete = String.join(" ", msgToSend);
+
+        channel.sendMessage(msgComplete).queue();
+        tc.sendMessageEmbeds(
+                        new EmbedBuilder()
+                                .setColor(Colors.NORMAL.getCode())
+                                .setTimestamp(ZonedDateTime.now())
+                                .setDescription("Message sent succesefully!")
+                                .addField("Channel", channel.getAsMention(), false)
+                                .addField("Message", msgComplete, false)
+                                .build())
+                .queue();
+    }
+
+    @Override
+    public ArrayList<Permission> getNeededPermissions() {
+        ArrayList<Permission> permissions = new ArrayList<>();
+        permissions.add(Permission.ADMINISTRATOR);
+        return permissions;
+    }
+}
